b0fdd6d4b45a23845ebd243e76aa3b16c999d836
Veljko Macuzic <veljkomac@gmail.com>
2016-02-21 18:56:41 +0100

    Initial commit

---
+++ src/main/java/com/vmac/app/App.java
@@ -0,0 +1,13 @@
+package com.vmac.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Dah Hello World!" );
+    }
+}
